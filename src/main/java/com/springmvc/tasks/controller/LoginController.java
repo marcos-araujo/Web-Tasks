234e2b017d22ac84c51a2a5fa19cb18922e794ad
Marcos Araujo <marcos.txt@gmail.com>
2020-06-04 22:27:08 -0300

    Renaming packages

--- src/main/java/com/springmvc/tasks/controller/LoginController.java
+++ src/main/java/com/springmvc/tasks/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.tasks.controller;
+package com.springmvc.tasks.controller;
 
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.tasks.controller.aux.Constant;
-import com.tasks.dao.UserDAO;
-import com.tasks.model.User;
+import com.springmvc.tasks.controller.aux.Constant;
+import com.springmvc.tasks.dao.UserDAO;
+import com.springmvc.tasks.model.User;
 
 @Transactional
 @Controller
--- src/main/java/com/springmvc/tasks/dao/TaskDAO.java
+++ src/main/java/com/springmvc/tasks/dao/TaskDAO.java
@@ -1,4 +1,4 @@
-package com.tasks.dao;
+package com.springmvc.tasks.dao;
 
 import java.util.Calendar;
 import java.util.List;
@@ -8,7 +8,7 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
-import com.tasks.model.Task;
+import com.springmvc.tasks.model.Task;
 
 @Repository
 public class TaskDAO{
